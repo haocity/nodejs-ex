9b6468d4dde154a05b62cc6138b1805c6a1cb901
疯狂减肥带 <ureygt@gmail.com>
2017-05-16 10:47:57 +0800

    test

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('views'));
 app.use(morgan('combined'))
-app.use(express.static('view'))
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
